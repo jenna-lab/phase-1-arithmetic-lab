3b30b5555a437b616c40e390f48bc9c6b422550f
jenna-lab <jennifer.sammy@student.moringaschool.com>
2022-11-23 22:28:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+let num3 = 19;
+let num4 = 5;
+const mod = num3 % num4;
+
+const max = (5, 10, 15, 20);
+Math.max(max);
+
+const random = Math.ceil(Math.random());
+console.log(random);
